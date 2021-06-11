3dd4752535199d1f9710f72ba5edc60a17d2692b
maryamh <maryamsaleem63@yahoo.com>
2021-06-11 15:36:03 +0500

    added husky package

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,12 @@
 module.exports = {
   env: {
-    browser: true,
     es2021: true,
+    node: true,
   },
-  extends: ['airbnb', 'prettier'],
-
+  extends: ['airbnb-base'],
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: ['prettier'],
-  rules: {
-    'prettier/prettier': 'error',
-  },
+  rules: {},
 };
